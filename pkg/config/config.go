f9854b3e9764b8efa10e2a3d1a899965ff651bce
subencheng <ben.su@conductorone.com>
2026-02-19 16:25:40 -0800

    add config

---
+++ pkg/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"github.com/conductorone/baton-sdk/pkg/field"
+)
+
+var (
+	accessTokenField = field.StringField(
+		"api-token",
+		field.WithDisplayName("API token"),
+		field.WithDescription("API token to authenticate buildkite"),
+		field.WithIsSecret(true),
+		field.WithRequired(true),
+	)
+
+	orgField = field.StringField(
+		"organization",
+		field.WithDisplayName("Organization"),
+		field.WithDescription("Buildkite Organization"),
+	)
+)
+
+//go:generate go run ./gen
+var Config = field.NewConfiguration(
+	[]field.SchemaField{
+		accessTokenField,
+		orgField,
+	},
+	field.WithConnectorDisplayName("Buildkite v2"),
+	field.WithHelpUrl("/docs/baton/buildkite-v2"),
+	field.WithIconUrl("/static/app-icons/buildkite.svg"),
+)
